2d45881841ac8d72fe465083d9862065433a1c32
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-03 17:36:28 -0400

    add new default colors

--- TeamFitnessApp/Formatting.swift
+++ TeamFitnessApp/Formatting.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIColor {
+    
+    open class var backgroundBlack: UIColor {
+        return UIColor(displayP3Red: 10, green: 10, blue: 30, alpha: 1)
+    }
+    
+    open class var backgroundImageBlue: UIColor {
+        return UIColor(displayP3Red: 0, green: 66, blue: 125, alpha: 1)
+    }
+    
+    open class var foregroundOrange: UIColor {
+        return UIColor(displayP3Red: 255, green: 162, blue: 0, alpha: 1)
+    }
+    
+}
+

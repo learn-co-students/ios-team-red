52c9ee4300d76ef7f93a0f82e702ef8df7837f3f
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-03 16:55:22 -0400

    first commit

---
+++ TeamFitnessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamFitnessApp
+//
+//  Created by Patrick O'Leary on 4/3/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

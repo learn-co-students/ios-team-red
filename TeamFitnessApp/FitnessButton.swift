c6b3797338a211aaf6e2f6aa047dfd0fae49aa04
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-04 10:44:57 -0400

    fix custom colors

---
+++ TeamFitnessApp/FitnessButton.swift
@@ -0,0 +1,21 @@
+//
+//  FitnessButton.swift
+//  TeamFitnessApp
+//
+//  Created by Patrick O'Leary on 4/4/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class FitnessButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- TeamFitnessApp/Formatting.swift
+++ TeamFitnessApp/Formatting.swift
@@ -12,15 +12,15 @@ import UIKit
 extension UIColor {
     
     open class var backgroundBlack: UIColor {
-        return UIColor(displayP3Red: 10, green: 10, blue: 30, alpha: 1)
+        return UIColor(displayP3Red: 10/255, green: 10/255, blue: 30/255, alpha: 1)
     }
     
     open class var backgroundImageBlue: UIColor {
-        return UIColor(displayP3Red: 0, green: 66, blue: 125, alpha: 1)
+        return UIColor(displayP3Red: 0/255, green: 66/255, blue: 125/255, alpha: 1)
     }
     
     open class var foregroundOrange: UIColor {
-        return UIColor(displayP3Red: 255, green: 162, blue: 0, alpha: 1)
+        return UIColor(displayP3Red: 255/255, green: 162/255, blue: 0/255, alpha: 1)
     }
     
 }

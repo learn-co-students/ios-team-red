9a00c96d900f1ffef356a03ba19fe4a4e78f4b83
almusto <ales.musto@gmail.com>
2017-04-05 16:59:25 -0400

    read total miles and steps between set dates and write height and weight to HealthKit

--- TeamFitnessApp/HealthKitManager.swift
+++ TeamFitnessApp/HealthKitManager.swift
@@ -44,7 +44,126 @@ final class HealthKidManager {
   }
 
 
-  //TODO: make methods to get healthkit data, make mehtods to write to healthkit
+  //MARK: - Read Data from HealthKit
+
+  func getSteps(fromDate startDate: Date, toDate endDate: Date?, completion: @escaping (Double?, Error?) -> ()) {
+    let now: Date!
+    if endDate != nil {
+      now = endDate
+    } else {
+      now = Date()
+    }
+
+    let past = startDate
+
+    var interval = DateComponents()
+    interval.day = 1
+
+    let sampleType = HKQuantityType.quantityType(forIdentifier: .stepCount)
+
+    let stepsQuery = HKStatisticsCollectionQuery(quantityType: sampleType!, quantitySamplePredicate: nil, options: .cumulativeSum, anchorDate: past, intervalComponents: interval)
+
+    stepsQuery.initialResultsHandler = { query, results, error in
+      DispatchQueue.main.async {
+        if error != nil {
+          print(error!.localizedDescription)
+          completion(nil, error)
+        }
+        let calendarSince = Calendar.current
+        let components = calendarSince.dateComponents([Calendar.Component.day, .hour], from: past, to: now)
+        var steps = 0.0
+        let hours = (components.day! * 24) + components.hour!
+        let startDate = calendarSince.date(byAdding: .hour, value: -hours, to: now)
+
+        if let myResults = results { myResults.enumerateStatistics(from: startDate!, to: now, with: { (statistics, stop) in
+          if let quantity = statistics.sumQuantity() {
+            steps += quantity.doubleValue(for: HKUnit.count())
+          }
+        })
+          completion(steps, nil)
+        }
+      }
+    }
+    healthStore.execute(stepsQuery)
+  }
+  
+
+  func getDistance(fromDate startDate: Date, toDate endDate: Date?, completion: @escaping (Double?, Error?) -> ()) {
+    let now: Date!
+    if endDate != nil {
+      now = endDate
+    } else {
+      now = Date()
+    }
+
+    let past = startDate
+
+    var interval = DateComponents()
+    interval.day = 1
+
+    let sampleType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)
+
+    let stepsQuery = HKStatisticsCollectionQuery(quantityType: sampleType!, quantitySamplePredicate: nil, options: .cumulativeSum, anchorDate: past, intervalComponents: interval)
+
+    stepsQuery.initialResultsHandler = { query, results, error in
+      DispatchQueue.main.async {
+        if error != nil {
+          print(error!.localizedDescription)
+          completion(nil, error)
+        }
+        let calendarSince = Calendar.current
+        let components = calendarSince.dateComponents([Calendar.Component.day, .hour], from: past, to: now)
+        var distance = 0.0
+        let hours = (components.day! * 24) + components.hour!
+        let startDate = calendarSince.date(byAdding: .hour, value: -hours, to: now)
+
+        if let myResults = results { myResults.enumerateStatistics(from: startDate!, to: now, with: { (statistics, stop) in
+          if let quantity = statistics.sumQuantity() {
+            distance += quantity.doubleValue(for: HKUnit.mile())
+          }
+        })
+          completion(distance, nil)
+        }
+      }
+    }
+    healthStore.execute(stepsQuery)
+  }
+
+
+  //MARK: - Write to HealthKit
+  func sendWeightToHealthKit(weight: Double, completion: @escaping (Bool) -> ()) {
+    let date = Date()
+    let weightType = HKQuantityType.quantityType(forIdentifier: .bodyMass)
+    let weightQuantity = HKQuantity(unit: HKUnit.pound(), doubleValue: weight)
+    let weightSample = HKQuantitySample(type: weightType!, quantity: weightQuantity, start: date, end: date)
+
+    healthStore.save(weightSample) { (success, error) in
+      if error != nil {
+        print(error!.localizedDescription)
+        completion(false)
+      } else {
+        completion(true)
+      }
+    }
+  }
+
+  func sendHeightToHealthKit(height: Double, completion: @escaping (Bool) -> ()) {
+    let date = Date()
+    let heightType = HKQuantityType.quantityType(forIdentifier: .height)
+    let heightQuantity = HKQuantity(unit: HKUnit.inch(), doubleValue: height)
+    let heightSample = HKQuantitySample(type: heightType!, quantity: heightQuantity, start: date, end: date)
+
+    healthStore.save(heightSample) { (success, error) in
+      if error != nil {
+        print(error!.localizedDescription)
+        completion(false)
+      } else {
+        completion(true)
+      }
+    }
+  }
+
+
 
 
 

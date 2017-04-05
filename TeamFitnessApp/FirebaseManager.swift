5864e08f3d5bf1bccdd27d01b30c98daecd70906
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-05 17:25:00 -0400

    add ability to put new teams and challenges to Firebase

--- TeamFitnessApp/FirebaseManager.swift
+++ TeamFitnessApp/FirebaseManager.swift
@@ -45,10 +45,6 @@ struct FirebaseManager {
             completion(.failedLogout("FirebaseManager could not log out user"))
         }
     }
-    
-    static func addNew(user: FIRUser) { //adds a new user's UID and email to the Firebase database
-        FirebaseManager.dataRef.child("users").child(user.uid).child("email").setValue(user.email)
-    }
 
 //save functions ************************************************************************************************************************************
     static func save(user: User) {// saves a user to the Firebase database
@@ -143,6 +139,7 @@ struct FirebaseManager {
         })
     }
     
+    //fetches a challenge from Firebase given a challenge id string, and returns the challenge through a closure
     static func fetchChallenge(withChallengeID challengeID: String, completion: @escaping (Challenge) -> Void) {
         dataRef.child("challenges").child(challengeID).observe(.value, with: { (snapshot) in
             if let challengeDict = snapshot.value as? [String: Any] {
@@ -151,44 +148,66 @@ struct FirebaseManager {
             }
         })
     }
+
+// addNew functions ******************************************************************************************************************************************
+    static func addNew(user: FIRUser) { //adds a new user's UID and email to the Firebase database
+        FirebaseManager.dataRef.child("users").child(user.uid).child("email").setValue(user.email)
+    }
+    
+    //adds an instance of Team to Firebase, and returns the auto generated teamID through a closure
+    static func addNew(team: Team, completion: (String) -> Void) {
+        let teamRef = dataRef.child("teams").childByAutoId()
+        let teamID = teamRef.key
+        
+        var usersDict = [String: Bool]()
+        var challengesDict = [String: Bool]()
+        
+        for user in team.userUIDs {
+            usersDict[user] = true
+        }
+        
+        for challenge in team.challengeIDs {
+            challengesDict[challenge] = true
+        }
+        
+        let post: [String: Any] = [
+            "captain": team.captain,
+            "users": usersDict,
+            "challenges": challengesDict,
+            "imageURL": team.imageURL
+        ]
+        teamRef.updateChildValues(post)
+        completion(teamID)
+    }
+    
+    static func addNew(challenge: Challenge, completion: (String) -> Void) {
+        let challengeRef = dataRef.child("challenges").childByAutoId()
+        let challengeID = challengeRef.key
+        
+        var usersDict = [String: Bool]()
+        
+        for user in challenge.userUIDs {
+            usersDict[user] = true
+        }
+        
+        let teamID = challenge.teamID ?? "no team"
+        let post: [String: Any] = [
+            "users": usersDict,
+            "creator": challenge.creator ?? nil,
+            "isPublic": challenge.isPublic ?? nil,
+            //            "startDate": String(challenge.startDate), TODO add function to the Challenge class that changes dates to string and vice versa
+            //            "endDate": String(challenge.endDate),
+            "team": [teamID: true]
+        ]
+        
+        challengeRef.updateChildValues(post)
+        completion(challengeID)
+    }
+    
     
 // test data ******************************************************************************************************************************************
     static func generateTestData() {
-        var testUser1 = User(name: "Test user 1", sex: "female", height: 75.5, weight: 125, teamIDs: [], challengeIDs: [], imageURL: "www.something.com", uid: "UID1234", email: "test1@test.com")
-        var testUser2 = User(name: "test User 2", sex: "male", height: 80.1, weight: 200, teamIDs: [], challengeIDs: [], imageURL: "www.somethingelse.comm", uid: "UID5678", email: "testuser2@test.com")
-        var testTeam1 = Team(userUIDs: [testUser1.uid, testUser2.uid], captain: testUser1, challengeIDs: [], imageURL: "www.cool.com", id: "team1UID1234")
-        var testTeam2 = Team(userUIDs: [testUser1.uid, testUser2.uid], captain: testUser2, challengeIDs: [], imageURL: "www.notcool.com", id: "team2UID5678")
-        let testChallenge1 = Challenge(startDate: Date(), endDate: Date(), goal: .caloriesBurned(2000), creator: testUser1, userUIDs: [testUser1.uid, testUser2.uid], isPublic: true, team: nil, id: "testChallenge2ID1234")
-        let testChallenge2 = Challenge(startDate: Date(), endDate: Date(), goal: .caloriesBurned(4000), creator: testUser2, userUIDs: [testUser1.uid, testUser2.uid], isPublic: false, team: testTeam1.id, id: "testChallenge2ID5678")
         
-        testUser1.teamIDs = [testTeam1.id, testTeam2.id]
-        testUser2.teamIDs = [testTeam1.id, testTeam2.id]
-        testUser1.challengeIDs = [testChallenge1.id, testChallenge2.id]
-        testUser2.challengeIDs = [testChallenge1.id, testChallenge2.id]
-        
-        testTeam1.challengeIDs = [testChallenge1.id, testChallenge2.id]
-        testTeam2.challengeIDs = [testChallenge1.id, testChallenge2.id]
-        
-        FirebaseManager.save(user: testUser1)
-        FirebaseManager.save(user: testUser2)
-        
-        FirebaseManager.save(team: testTeam1)
-        FirebaseManager.save(team: testTeam2)
-        
-        FirebaseManager.save(challenge: testChallenge1)
-        FirebaseManager.save(challenge: testChallenge2)
-        
-        fetchUser(withUID: "UID1234") { (user) in
-            print("User fetched: \(user.uid), \(user.email), \(user.height), \(user.name)")
-        }
-        
-        fetchTeam(withTeamID: "team1UID1234") { (team) in
-            print("Team fetched: \(team.id)")
-        }
-        
-        fetchChallenge(withChallengeID: "testChallenge2ID1234") { (challenge) in
-            print("Challenge fetched: \(challenge.id)")
-        }
         
     }
 }

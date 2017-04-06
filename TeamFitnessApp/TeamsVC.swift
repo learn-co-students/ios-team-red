8b8661c7ae0810045757ca7a3d2856183b55b9c3
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-06 11:46:03 -0400

    add labels and tableView

--- TeamFitnessApp/TeamsVC.swift
+++ TeamFitnessApp/TeamsVC.swift
@@ -11,10 +11,15 @@ import UIKit
 class TeamsVC: UIViewController {
     
     let screenBounds = UIScreen.main.bounds
+    let mainView = FitnessView()
+    let titleLabel = FitnessLabel()
+    let myTeamsLabel = FitnessLabel()
+    let myTeamsView = UITableView()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupSubViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,8 +39,44 @@ class TeamsVC: UIViewController {
     */
     
     func setupSubViews() {
-        self.view = FitnessView(frame: screenBounds)//change the primary view in the view controller to be a FitnessView
-
+        self.view = mainView
+        setupTitle()
+        setUpMyTeams()
+    }
+    
+    func setupTitle() {
+        self.view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+        titleLabel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
+        titleLabel.textAlignment = .center
+        titleLabel.changeFontSize(to: 28)
+        titleLabel.reverseColors()
+        titleLabel.text = "Teams"
+    }
+    
+    func setUpMyTeams() {
+        view.addSubview(myTeamsLabel)
+        myTeamsLabel.translatesAutoresizingMaskIntoConstraints = false
+        myTeamsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myTeamsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        myTeamsLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+        myTeamsLabel.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
+        myTeamsLabel.textAlignment = .center
+        myTeamsLabel.changeFontSize(to: 20)
+        myTeamsLabel.reverseColors()
+        myTeamsLabel.text = "My Teams:"
+        
+        view.addSubview(myTeamsView)
+        myTeamsView.translatesAutoresizingMaskIntoConstraints = false
+        myTeamsView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myTeamsView.topAnchor.constraint(equalTo: myTeamsLabel.bottomAnchor).isActive = true
+        myTeamsView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        myTeamsView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        myTeamsView.backgroundColor = UIColor.green
+        
     }
 
 }

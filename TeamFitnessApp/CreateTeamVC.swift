108148db3b659f7e38d91f0b08072917c328f35f
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-07 14:34:03 -0400

    new branch first commit

---
+++ TeamFitnessApp/CreateTeamVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateTeamVC.swift
+//  TeamFitnessApp
+//
+//  Created by Patrick O'Leary on 4/7/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class CreateTeamVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

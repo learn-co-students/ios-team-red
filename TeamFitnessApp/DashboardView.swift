b00bbb742fd59776415a56e381a5fbed75d88940
almusto <ales.musto@gmail.com>
2017-04-09 17:24:29 -0400

    add custom bar chart and pie chart for tracking

---
+++ TeamFitnessApp/DashboardView.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardView.swift
+//  TeamFitnessApp
+//
+//  Created by Alessandro Musto on 4/9/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+
+class DashboardView: FitnessView {
+
+  var imageView: UIImageView!
+  
+
+//  override init(frame: CGRect) {
+//    super.init(coder: frame)
+//
+//
+//  }
+
+
+
+
+
+
+
+}

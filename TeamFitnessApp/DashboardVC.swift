f1e59e118927a77aaf376bd3b518b9a01186164d
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-11 15:52:48 -0400

    fix merge errors

--- TeamFitnessApp/DashboardVC.swift
+++ TeamFitnessApp/DashboardVC.swift
@@ -40,8 +40,9 @@ class DashboardVC: UIViewController {
     let goal = Goal(type: .exerciseTime, value: 60)
     let goal1 = Goal(type: .caloriesBurned, value: 800)
     let goals = [goal, goal1]
+    
+    testUser = User(name: "Captain Planet", sex: "Male", height: 80, weight: 300, teamIDs: [], challengeIDs: ["-Kh-2VOryz2zuH2ht90j", "-Kh-4CnhvvQ7GCkJHUOM"], goals: goals, email: "captainplanet@hesahero.com")
 
-    testUser = User(name: "Sandro", sex: "male", height: 62, weight: 240, teamIDs: [], challengeIDs: ["-Kh-2VOryz2zuH2ht90j", "-Kh-4CnhvvQ7GCkJHUOM"], imageURL: "", uid: "ueIVp3UT2mVJHwVT8Pgoz0GPfbK2", email: "ales.musto@gmail.com", goals: goals)
   }
 
   func getChallenges() {

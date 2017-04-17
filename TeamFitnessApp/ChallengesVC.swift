72e2cf302cf174da3ab7fc2a3412b05c875b21a3
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-17 13:17:35 -0400

    commit before new branch

--- TeamFitnessApp/ChallengesVC.swift
+++ TeamFitnessApp/ChallengesVC.swift
@@ -47,17 +47,6 @@ class ChallengesVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }

7aa74639c06fca963db3805557168a881804c22a
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-20 12:26:39 -0400

    new branch first commit

---
+++ TeamFitnessApp/ReportButton.swift
@@ -0,0 +1,21 @@
+//
+//  ReportButton.swift
+//  TeamFitnessApp
+//
+//  Created by Patrick O'Leary on 4/20/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ReportButton: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+}
